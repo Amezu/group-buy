5ca1ac0e8c7c615331dbccf51cf5c5a8678ae977
Julia <juliaaa007@gmail.com>
2019-09-07 00:35:26 +0200

    Load people list in AddPersonActivity

--- app/src/main/java/com/example/groupbuy/party/AddPersonActivity.java
+++ app/src/main/java/com/example/groupbuy/party/AddPersonActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +22,18 @@ public class AddPersonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
+        loadPeopleList();
+    }
+
+    private void loadPeopleList() {
+        String[] people = {"Ashely", "Devin", "Ivan", "Gavin", "Lev", "Damon", "Lillian", "Kyra", "Forrest", "Owen", "Hayden", "Nash", "Dieter", "Holly", "Victor", "Aline", "Dominic", "Jennifer", "Logan"};
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, people);
+        ListView view = findViewById(R.id.list);
+        view.setAdapter(adapter);
     }
 
     public void addPerson(View view) {
-        TextView nameView = (TextView) findViewById(R.id.personEdit);
+        TextView nameView = findViewById(R.id.edit);
         String name = nameView.getText().toString();
         Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
         new HttpRequestDebug(this).addPersonToParty(createHashMap(name));
