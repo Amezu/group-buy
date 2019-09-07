fa073015f11b2c895c890c0b5f8979aeff6a9e3f
Julia <juliaaa007@gmail.com>
2019-09-07 13:22:19 +0200

    Refactor PartyListFragment

---
+++ app/src/main/java/com/example/groupbuy/connection/JsonParser.java
@@ -0,0 +1,25 @@
+package com.example.groupbuy.connection;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonParser {
+    public static List<String> parsePartyList(JSONObject s) {
+        List<String> parties = new ArrayList<>();
+        try {
+            JSONArray array = s.getJSONArray("groupList");
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                parties.add(object.getString("groupName"));
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return parties;
+    }
+}
