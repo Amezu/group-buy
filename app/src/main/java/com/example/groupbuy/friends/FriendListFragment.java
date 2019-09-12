a439cc40f2bedb96b5f25e38193be50d313bb82c
Julia <juliaaa007@gmail.com>
2019-09-13 00:32:06 +0200

    Add context menu for friend list

--- app/src/main/java/com/example/groupbuy/friends/FriendListFragment.java
+++ app/src/main/java/com/example/groupbuy/friends/FriendListFragment.java
@@ -2,12 +2,17 @@ package com.example.groupbuy.friends;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -40,6 +45,32 @@ public class FriendListFragment extends Fragment {
         ListAdapter adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, people);
         ListView view = getView().findViewById(R.id.list);
         view.setAdapter(adapter);
+        registerForContextMenu(view);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.list) {
+            ListView lv = (ListView) v;
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            String obj = (String) lv.getItemAtPosition(acmi.position);
+
+            menu.setHeaderTitle(obj);
+            menu.add("Delete");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        TextView view = (TextView) info.targetView;
+
+        Toast.makeText(getActivity(), item.getTitle() + " " + view.getText(), Toast.LENGTH_SHORT).show();
+        if (item.getTitle() == "Delete") {
+//            TODO: Send request
+        } else return false;
+
+        return true;
     }
 
     private void openAddFriendActivity() {
